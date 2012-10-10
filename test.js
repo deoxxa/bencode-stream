d0c202ad76dc8a4c7216e20fa1d71f78705e7605
deoxxa <deoxxa@fknsrs.biz>
2012-10-11 00:23:26 +1100

    make sure we don't read outside our buffer

--- test.js
+++ test.js
@@ -8,4 +8,7 @@ decoder.on("data", function(data) {
   console.log(data);
 });
 
-decoder.write("d1:ai1234e1:bli1ei2eee");
+decoder.write("d1");
+decoder.write(":");
+decoder.write("ai12");
+decoder.write("34e1:bli1ei2eee");
